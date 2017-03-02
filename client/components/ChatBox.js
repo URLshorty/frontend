1b8093e21616ef4f2fed6f7ae545ab94a8f803e5
NoneOfMaster <tepper.russell@gmail.com>
2017-03-02 15:16:51 -0500

    Styles and sample functions for chat box

--- client/components/ChatBox.js
+++ client/components/ChatBox.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
+import sampleChat from '../data/sampleChat'
+
 export default class ChatBox extends React.Component {
-  constructor(props) {
-    super(props);
+
+  handleSubmit(e) {
+    e.preventDefault()
+    document.getElementById("chat-window").innerHTML += `<p>${this.refs.message.value}</p>`
+    document.getElementById("chat-window").scrollTop = document.getElementById("chat-window").scrollHeight
+    this.refs.message.value = "";
   }
 
   render() {
     return (
-      <div className="chat-box">
-        this a chat box
+      <div id="chat-box">
+        <div id="chat-window">{sampleChat.map((x)=> <p>{x}</p> )}</div>
+        <form 
+          onSubmit={this.handleSubmit.bind(this)}
+          id="message-form"
+        >
+          <input 
+            type="text" 
+            ref="message" 
+            placeholder="type message here"
+          />
+
+          <input type="submit" hidden />
+
+        </form>
       </div>
     );
   }
