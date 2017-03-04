be29d8d4424cbf1c6543e61e698e322be641dc9f
NoneOfMaster <tepper.russell@gmail.com>
2017-03-04 14:47:35 -0500

    Modal inner stylings

---
+++ client/components/LoginModal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default class LoginModal extends React.Component {
+
+  render() {
+    return (
+      <div className="modal-backdrop">
+        <div className="modal">
+          <div className="modal-header">
+            <h1 className="login">Log In</h1>
+          </div>
+
+          <div className="modal-content">
+            login momomomomomomomomomomomomomomomomomomomomoasdfasdfadsfadsdal
+          </div>
+
+          <div className="modal-footer"></div>
+        </div>
+      </div>
+    );
+  }
+}
--- client/components/Main.js
+++ client/components/Main.js
@@ -18,7 +18,7 @@ export default class Main extends React.Component {
         {/* because just this.props.childen doesn't get you access to the children's props  */}
         {React.cloneElement(this.props.children, this.props)} {/* study what's going on here more */}
 
-        <div id="footer"><div>Thanks for visiting!</div></div>
+        <div id="footer">Thanks for visiting!</div>
 
       </div>
     );
