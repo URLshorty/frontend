d16b91979ea130f459f5cde1188c7e7ba3d51b4a
NoneOfMaster <tepper.russell@gmail.com>
2017-03-04 17:18:28 -0500

    Finished login modal - left-off commit from previous merge with master

--- client/components/ModalConductor.js
+++ client/components/ModalConductor.js
@@ -7,10 +7,21 @@ export default class ModalConductor extends React.Component {
   renderModal(modal) {
     switch (this.props.currentModal) {
       case 'login':
-        return <LoginModal />
-
+        this.setCloseModal() // why doesn't this cause a memory leak
+        // class set here on React DOM not responsive to its CSS, As a result modal-backdrop el is in LoginModal etc. (research)
+        return <LoginModal className="modal-backdrop" /> 
       default:
         return null
+    }
+  }
+
+  setCloseModal() {
+    window.onclick = (e) => {
+      let modalBackdrop = document.getElementsByClassName("modal-backdrop")[0]
+      if (e.target === modalBackdrop) {
+        this.props.setCurrentModal(null)
+      }
+
     }
   }
 
