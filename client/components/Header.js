91d389db06a258cab84c7ba0aa11f7695703f726
NoneOfMaster <tepper.russell@gmail.com>
2017-03-04 22:10:06 -0500

    Inspiration struck: started complete redesign :fire:

--- client/components/Header.js
+++ client/components/Header.js
@@ -13,16 +13,16 @@ export default class Header extends React.Component {
     return (
       <div id="header">
         
+        <div className="logo">
+          <Link to="/"><span>URL</span>.shorty</Link>
+        </div>
+
         <div id="nav-bar">
           <IndexLink to="/" className="nav-link" activeClassName="active">Home</IndexLink>
           <NavLink onClick={this.showModal.bind(this, "loginModal")} className="nav-link" activeClassName="active">Login</NavLink>
           <NavLink onClick={this.showModal.bind(this, "signUpModal")} className="nav-link" activeClassName="active">Sign Up</NavLink>
           <NavLink to="/users/random-username" className="nav-link" activeClassName="active">Random User Profile</NavLink>
         </div>
-
-        <h1 className="logo">
-          <Link to="/"><span>URL</span>.shorty</Link>
-        </h1>
 
       </div>
     )
