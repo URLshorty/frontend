f72112696066f939464e5086368300bc1c418149
NoneOfMaster <tepper.russell@gmail.com>
2017-04-20 18:44:03 -0400

    Added isFetching prop to UserProfile to wait for retrieveData()

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -14,6 +14,7 @@ export default class UserProfile extends React.Component {
       created_at: ' ', // mandatory space so not undefined
       mostPopularLong: '',
       mostPopularShort: '',
+      isFetching: true,
     }
   }
 
@@ -24,6 +25,10 @@ export default class UserProfile extends React.Component {
   componentWillReceiveProps(nextProps) {
     if ( this.props.params.id != nextProps.params.id ||
          this.props.hook != nextProps.hook ) {
+      this.setState({
+        ...this.state,
+        isFetching: true,
+      })
       this.retrieveData(nextProps.params.id)
     }
   }
@@ -49,6 +54,7 @@ export default class UserProfile extends React.Component {
             mostPopularLong: json.mostPopularLong,
             mostPopularShort: json.mostPopularShort,
             picture_id: json.picture_id,
+            isFetching: false,
           })
         }
       })
@@ -139,68 +145,75 @@ export default class UserProfile extends React.Component {
     return (
       <div id="user-profile">
 
-        { this.state.picture_id ? (
+        { !this.state.isFetching &&
+
           <div>
-            <img
-              id="profile-picture"
-              src={`${process.env.API_URL}/uploads/${this.state.picture_id}`} />
-            { this.props.user.id == this.props.params.id &&
-              <button onClick={this.uploadModal.bind(this)}>CHANGE</button>
-            }
-          </div>
-        ) : (
-          <div>
-            <div className="user-icon user-icon-white" />
-            { this.props.user.id == this.props.params.id &&
-              <button onClick={this.uploadModal.bind(this)}>UPLOAD</button>
-            }
-          </div>
-        )}
 
-        <div id="user-data">
+            { this.state.picture_id ? (
+              <div>
+                <img
+                  id="profile-picture"
+                  src={`${process.env.API_URL}/uploads/${this.state.picture_id}`} />
+                { this.props.user.id == this.props.params.id &&
+                  <button onClick={this.uploadModal.bind(this)}>CHANGE</button>
+                }
+              </div>
+            ) : (
+              <div>
+                <div className="user-icon user-icon-white" />
+                { this.props.user.id == this.props.params.id &&
+                  <button onClick={this.uploadModal.bind(this)}>UPLOAD</button>
+                }
+              </div>
+            )}
 
-          <SingleFieldForm
-            name="username"
-            value={this.state.username}
-            profileId={this.state.id}
-            currentUser={this.props.user}
-            submit={this.newUsernameSubmit}
-            setCurrentModal={this.props.setCurrentModal}
-            updateUserData={this.props.updateUserData} />
+            <div id="user-data">
 
-        {/* update parent state method needs to be passed down to update props */}
+              <SingleFieldForm
+                name="username"
+                value={this.state.username}
+                profileId={this.state.id}
+                currentUser={this.props.user}
+                submit={this.newUsernameSubmit}
+                setCurrentModal={this.props.setCurrentModal}
+                updateUserData={this.props.updateUserData} />
 
-        <SingleFieldForm
-            name="email"
-            value={this.state.email}
-            profileId={this.state.id}
-            currentUser={this.props.user}
-            submit={this.newEmailSubmit}
-            setCurrentModal={this.props.setCurrentModal}
-            updateUserData={this.props.updateUserData} />
+              <SingleFieldForm
+                  name="email"
+                  value={this.state.email}
+                  profileId={this.state.id}
+                  currentUser={this.props.user}
+                  submit={this.newEmailSubmit}
+                  setCurrentModal={this.props.setCurrentModal}
+                  updateUserData={this.props.updateUserData} />
 
-          <div id="user-created-date">
-            <span>User Since</span><br />
-            <span>
-              {this.state.created_at.slice(0, this.state.created_at.indexOf('T'))}
-            </span>
-          </div>
+                <div id="user-created-date">
+                  <span>User Since</span><br />
+                  <span>
+                    {this.state.created_at.slice(0, this.state.created_at.indexOf('T'))}
+                  </span>
+                </div>
 
-          { this.state.mostPopularShort &&
+              { this.state.mostPopularShort &&
 
-            <div id="most-popular-link">
-              <span>most popular link</span><br />
-              <span><a
-                href={`${process.env.API_URL}/${this.state.mostPopularShort}`}
-                target="_bland" >
-                {`${process.env.API_URL}/${this.state.mostPopularShort}`.slice(7)}
-                </a></span><br />
-              <span>{this.state.mostPopularLong}</span>
+                <div id="most-popular-link">
+                  <span>most popular link</span><br />
+                  <span><a
+                    href={`${process.env.API_URL}/${this.state.mostPopularShort}`}
+                    target="_bland" >
+                    {`${process.env.API_URL}/${this.state.mostPopularShort}`.slice(7)}
+                    </a></span><br />
+                  <span>{this.state.mostPopularLong}</span>
+                </div>
+
+              }
+
             </div>
 
-          }
+          </div>
 
-        </div>
+        }
+
       </div>
     )
   }
